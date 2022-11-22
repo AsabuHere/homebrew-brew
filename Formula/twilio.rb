edfedb3dc08734684e53560b2f90081c46f2f560
Athira Sabu <102021496+AsabuHere@users.noreply.github.com>
2022-11-22 10:41:07 +0530

    Update twilio.rb

--- Formula/twilio.rb
+++ Formula/twilio.rb
@@ -3,7 +3,7 @@ require "language/node"
 class Twilio < Formula
   desc "unleash the power of Twilio from your command prompt"
   homepage "https://github.com/twilio/twilio-cli"
-  url "https://twilio-cli-prod.s3.amazonaws.com/twilio-v1.1.1/twilio-v1.1.1.tar.gz"
+  url "https://twilio-cli-dev.s3.amazonaws.com/twilio-v1.1.1/twilio-v1.1.1.tar.gz"
   version "1.1.1"
   sha256 "e03706a2887cc25786f9c08b46750b9177978646d9747e732836f986f232dea5"
   depends_on "node"
